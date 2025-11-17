36994164e7801404c35cd087dd4013fc7bf42465
nimra <snimramukhtar321@gmail.com>
2025-11-17 05:54:51 +0500

    code update

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,17 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import clientRoutes from "./routes/clientRoutes.js";
+import { createServer } from "http"; // Only used for Vercel edge workaround
+import { Server } from "http";
+
 dotenv.config();
 connectDB();
+
 const app = express();
+
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], 
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
   })
 );
 app.use(express.json());
@@ -21,3 +26,6 @@ app.use("/api/clients", clientRoutes);
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+// 👇 THIS IS THE IMPORTANT PART:
+export default app;
